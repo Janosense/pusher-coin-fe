45bf40a7a575ed9132f37660775824f8b6c51c3f
Tymofii Synianskyi <janosense@gmail.com>
2025-10-13 23:05:46 +0300

    Log environment mode during runtime for dev and production builds
    - Add console logs to indicate the current environment mode (`Dev mode` or `PROD mode`)
    - Ensure code is tree-shaken in production builds

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@ import NavigationToggle from '@/components/NavigationToggle.vue'
 import Navigation from '@/components/Navigation.vue'
 
 const route = useRoute()
+if (import.meta.env) {
+  // code inside here will be tree-shaken in production builds
+  console.log('Dev mode')
+}else {
+  console.log('PROD mode')
+}
 </script>
 
 <template>
